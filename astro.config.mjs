3c8c7e9a6eaf0210e8164c071d6ec7c0d5f850e2
Pierre Prézelin <pierreprezelin.pro@gmail.com>
2024-06-03 22:16:32 -0400

    Added Tailwind config and auto-generated sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://www.audreylecuona.com',
+	integrations: [
+		sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
