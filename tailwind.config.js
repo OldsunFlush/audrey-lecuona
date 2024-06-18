d3bc4880c02b446bb3087228ca0caa374331810d
Pierre Prézelin <pierreprezelin.pro@gmail.com>
2024-06-18 15:57:09 -0400

    Import fonts locally

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#3D5361",
-    },
-    container: {
-      center: true,
-      padding: "1.5rem",
+    extend: {
+      fontFamily: {
+        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+        serif: ["PT Serif", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        primary: "#3D5361",
+      },
+      container: {
+        center: true,
+        padding: "1.5rem",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
